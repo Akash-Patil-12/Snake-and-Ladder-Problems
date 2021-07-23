0f8238fb7e3477b9b847251aa90c02277d4e67f9
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-23 16:01:49 +0530

    [Akash] Add : Play the game with 2 player and report which player won the game

--- Snake-and-Ladder/Play.cs
+++ Snake-and-Ladder/Play.cs
@@ -7,40 +7,99 @@ namespace Snake_and_Ladder
     class Play
     {
         //variables
-        int position = 0, dice, option, countDice = 0;
+        static int positionPlayer1 = 0;
+        static int positionPlayer2 = 0;
+        static int countDice = 0;
         //constants
-        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, RESTART = 0, WIN = 100, START = 0;
+        const int WIN = 100, NO_PLAY = 0, LADDER = 1, SNAKE = 2;
         /// <summary>
-        /// Roll the dies till it reach exact postion = 100 and it also show count dice roll 
+        /// Randombly call Player 1 or 2 and display who won the game
         /// </summary>
         public void Start()
+        {
+            while (true)
+            {
+                Random random = new Random();
+                int player = random.Next(1, 3);
+                if (player == 1)
+                    startPlayer1();
+                else
+                    startPlayer2();
+                if (positionPlayer1 == WIN || positionPlayer2 == WIN)
+                    break;
+                
+            }
+            Console.WriteLine("position of player 1 :" + positionPlayer1);
+            Console.WriteLine("position of player 2 :" + positionPlayer2);
+            if (positionPlayer1 > positionPlayer2)
+                Console.WriteLine("Player 1 is won");
+            else
+                Console.WriteLine("Player 2 is won");
+        }
+        /// <summary>
+        /// Perform player 1 operation
+        /// </summary>
+        static void startPlayer1()
         {
             Random random = new Random();
             while (true)
             {
-                if (position == WIN)
-                    break;
-                if (position < RESTART)
-                    position = START;
-                dice = random.Next(1, 7);
-                countDice++;
-                option = random.Next(0, 3);
+                int dice = random.Next(1, 7);
+                countDice += 1;
+                int option = random.Next(0, 3);
                 switch (option)
                 {
                     case NO_PLAY:
                         break;
                     case LADDER:
-                        if ((position + dice) <= 100)
-                            position += dice;
+                        if ((positionPlayer1 + dice) <= 100)
+                            positionPlayer1 += dice;
                         break;
                     case SNAKE:
-                        position -= dice;
+                        positionPlayer1 -= dice;
                         break;
                     default:
                         break;
                 }
-                Console.WriteLine("Count dice is :" + countDice);
-                Console.WriteLine("Postion :" + position);
+                if (positionPlayer1 < 0)
+                    positionPlayer1 = 0;
+                Console.WriteLine("Dice 1 count to win the game is :" + countDice);
+                Console.WriteLine("PostionPlayer1 :" + positionPlayer1);
+                if(option!=1)
+                break;
+            }
+        }
+        /// <summary>
+        /// perform player 2 operation
+        /// </summary>
+        static void startPlayer2()
+        {
+            Random random = new Random();
+            while (true)
+            {
+                int dice = random.Next(1, 7);
+                countDice += 1;
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NO_PLAY:
+                        break;
+                    case LADDER:
+                        if ((positionPlayer2 + dice) <= 100)
+                            positionPlayer2 += dice;
+                        break;
+                    case SNAKE:
+                        positionPlayer2 -= dice;
+                        break;
+                    default:
+                        break;
+                }
+                if (positionPlayer2 < 0)
+                    positionPlayer2 = 0;
+                Console.WriteLine("Dice 2 count to win the game is :" + countDice);
+                Console.WriteLine("PostionPlayer2 :" + positionPlayer2);
+                if(option!=1)
+                break;
             }
         }
     }
