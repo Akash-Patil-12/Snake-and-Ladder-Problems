53bcae563a3aab14a1a861549c7d3986d15c8538
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-23 14:59:44 +0530

    [Akash] Add : Repeat till player reaches the winning position 100

--- Snake-and-Ladder/Play.cs
+++ Snake-and-Ladder/Play.cs
@@ -7,29 +7,34 @@ namespace Snake_and_Ladder
     class Play
     {
         //variables
-        int position = 0;
+        int position = 0, dice, option;
         //constants
-        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, RESTART = 0, WIN = 100;
         /// <summary>
-        /// Roll the dies and add value according the option to position 
+        /// Roll the dies till it reach 100 and add value according the option to position 
         /// </summary>
         public void Start()
         {
-            Random random = new Random();
-            int dice = random.Next(1, 7);
-            int option = random.Next(0, 3);
-            switch (option)
+            while (position <= WIN)
             {
-                case NO_PLAY:
-                    break;
-                case LADDER:
-                    position += dice;
-                    break;
-                case SNAKE:
-                    position -= dice;
-                    break;
-                default:
-                    break;
+                Random random = new Random();
+                dice = random.Next(1, 7);
+                option = random.Next(0, 3);
+                if (position < RESTART)
+                    position = 0;
+                switch (option)
+                {
+                    case NO_PLAY:
+                        break;
+                    case LADDER:
+                        position += dice;
+                        break;
+                    case SNAKE:
+                        position -= dice;
+                        break;
+                    default:
+                        break;
+                }
             }
             Console.WriteLine("Postion :" + position);
         }
